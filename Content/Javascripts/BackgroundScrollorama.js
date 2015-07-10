f4e6d9234c3d749c1550e72bc8c85b40fceb5c02
calmsen <icalmsen@gmail.com>
2015-07-10 14:11:26 +1100

    incapsulate to module

--- Content/Javascripts/BackgroundScrollorama.js
+++ Content/Javascripts/BackgroundScrollorama.js
@@ -1,109 +1,107 @@
-function initScrollorama(
-    c1Height, c2Height, c3Height, c4Height, c5Height, 
-    bg1PartSel, bg2PartSel, bg3PartSel, bg4PartSel, bg5PartSel,
-    duration
-    ) {
-    var delta = duration;
-    var c1Top = 0;
-    var c2Top = c1Top + c1Height;
-    var c3Top = c2Top + c2Height;
-    var c4Top = c3Top + c3Height;
-    var c5Top = c4Top + c4Height;
-    var bg1FadeOutEl = $(bg1PartSel + "-fadeout");
-    var bg2FadeInEl = $(bg2PartSel + "-fadein");
-    var bg2FadeOutEl = $(bg2PartSel + "-fadeout");
-    var bg3FadeInEl = $(bg3PartSel + "-fadein");
-    var bg3FadeOutEl = $(bg3PartSel + "-fadeout");
-    var bg4FadeInEl = $(bg4PartSel + "-fadein");
-    var bg4FadeOutEl = $(bg4PartSel + "-fadeout");
-    var bg5FadeInEl = $(bg5PartSel + "-fadein");
-    var bg5FadeOutEl = $(bg5PartSel + "-fadeout");
-
-    winEl.on("scroll", function () {
-        if (winEl.scrollTop() > c2Top) {
-            bg2FadeInEl.css("display", "block");
-            bg2FadeOutEl.css("display", "none");
+var BackgroundScrollorama = (function($){
+	function BackgroundScrollorama() {
+        this.init.apply(this, arguments);
+    }
+	var winEl = $(window);
+	var uniqId = 0;
+    $.extend(BackgroundScrollorama.prototype, {
+        defaults: {
+            duration: 300
+			, delta: 300
+			, _bgElements: [] 
+			, _scrollorama: null
         }
-        if (winEl.scrollTop() > (c2Top + (c2Height / 4))) {
-            bg2FadeInEl.css("display", "none");
-            bg2FadeOutEl.css("display", "block");
-        }
-        //
-        if (winEl.scrollTop() > c3Top) {
-            bg3FadeInEl.css("display", "block");
-            bg3FadeOutEl.css("display", "none");
-        }
-        if (winEl.scrollTop() > (c3Top + (c3Height / 4))) {
-            bg3FadeInEl.css("display", "none");
-            bg3FadeOutEl.css("display", "block");
-        }
-        //
-        if (winEl.scrollTop() > c4Top) {
-            bg4FadeInEl.css("display", "block");
-            bg4FadeOutEl.css("display", "none");
-        }
-        if (winEl.scrollTop() > (c4Top + (c4Height / 4))) {
-            bg4FadeInEl.css("display", "none");
-            bg4FadeOutEl.css("display", "block");
-        }
-    });
-
-    
-
-    scrollorama.animate(bg1FadeOutEl, {
-        delay: c1Top + (c1Height - 2 * duration),
-        duration: duration,
-        property: 'opacity',
-        start: 1,
-        end: 0
-    });
-    scrollorama.animate(bg2FadeInEl, {
-        delay: c1Top + (c1Height - duration) - delta,
-        duration: duration + delta,
-        property: 'opacity',
-        start: 0,
-        end: 1
-    });
-    scrollorama.animate(bg2FadeOutEl, {
-        delay: c2Top + (c2Height - 2 * duration),
-        duration: duration,
-        property: 'opacity',
-        start: 1,
-        end: 0
-    });
-    scrollorama.animate(bg3FadeInEl, {
-        delay: c2Top + (c2Height - duration) - delta,
-        duration: duration + delta,
-        property: 'opacity',
-        start: 0,
-        end: 1
-    });
-    scrollorama.animate(bg3FadeOutEl, {
-        delay: c3Top + (c3Height - 2 * duration),
-        duration: duration,
-        property: 'opacity',
-        start: 1,
-        end: 0
-    });
-    scrollorama.animate(bg4FadeInEl, {
-        delay: c3Top + (c3Height - duration) - delta,
-        duration: duration + delta,
-        property: 'opacity',
-        start: 0,
-        end: 1
-    });
-    scrollorama.animate(bg4FadeOutEl, {
-        delay: c4Top + (c4Height - 2 * duration),
-        duration: duration,
-        property: 'opacity',
-        start: 1,
-        end: 0
-    });
-    scrollorama.animate(bg5FadeInEl, {
-        delay: c4Top + (c4Height - duration) - delta,
-        duration: duration + delta,
-        property: 'opacity',
-        start: 0,
-        end: 1
-    });
-}
+        , init: function (options) {
+			$.extend(true, this, this.defaults, options);
+			// smooth scroll
+			if ($('html').hasClass('desktop')) {
+				$.srSmoothscroll({
+					step: 100,
+					speed: 500
+				});
+			}
+			//
+			this._scrollorama = $.scrollorama({
+				blocks: '.scrollblock'
+			});
+		}
+		, addBgElements: function(arr) {
+			for (var i in arr) 
+				this.addBgElement(arr[i]);
+		}
+		, addBgElement: function(o) {
+			var cHeight = o.cHeight, 
+				cBackgroundImageSrc = o.cBackgroundImageSrc
+				scrollblockInnerSel = o.scrollblockInnerSel;
+				
+			var lastBgElement = this._bgElements.length > 0 
+								? this._bgElements[this._bgElements.length - 1]
+								: null;
+			var bgElementId = uniqId++;
+			var cTop = 0;
+			if (lastBgElement != null) {
+				cTop = lastBgElement.cTop + lastBgElement.cHeight;
+			}
+			//
+			var bgFadeInEl = null;
+			if (lastBgElement != null) {
+				bgFadeInEl = $("<div/>", {
+					id: "bg-" + bgElementId + "-fadein"
+					, "class": "bg"
+				})
+				.css({"background-image": "url(" + cBackgroundImageSrc + ")"});
+			}
+			var bgFadeOutEl = $("<div/>", {
+				id: "bg-" + bgElementId + "-fadeout"
+				, "class": "bg"
+			})
+			.css({"background-image": "url(" + cBackgroundImageSrc + ")", display: (lastBgElement != null ? "none" : "block")});
+			
+			var bgElement = {
+				Id: bgElementId
+				, cTop: cTop
+				, cHeight: cHeight
+				, scrollblockInnerSel: scrollblockInnerSel
+				, bgFadeInEl: bgFadeInEl
+				, bgFadeOutEl: bgFadeOutEl
+			};
+			this._bgElements.push(bgElement);
+			//
+			if (bgFadeInEl != null)
+				bgFadeInEl.appendTo(scrollblockInnerSel);
+			bgFadeOutEl.appendTo(scrollblockInnerSel);
+			if (bgFadeInEl != null) {
+				winEl.on("scroll", function () {
+					if (winEl.scrollTop() > cTop) {
+						bgFadeInEl.css("display", "block");
+						bgFadeOutEl.css("display", "none");
+					}
+					if (winEl.scrollTop() > (cTop + (cHeight / 4))) {
+						bgFadeInEl.css("display", "none");
+						bgFadeOutEl.css("display", "block");
+					}
+				});
+				//
+				
+			}
+			if (lastBgElement != null) {
+				this._scrollorama.animate(lastBgElement.bgFadeOutEl, {
+					delay: lastBgElement.cTop + (lastBgElement.cHeight - 2 * this.duration),
+					duration: this.duration,
+					property: 'opacity',
+					start: 1,
+					end: 0
+				});
+				this._scrollorama.animate(bgFadeInEl, {
+					delay: lastBgElement.cTop + (lastBgElement.cHeight - this.duration) - this.delta,
+					duration: this.duration + this.delta,
+					property: 'opacity',
+					start: 0,
+					end: 1
+				});
+			}
+			
+		}
+	});
+	return BackgroundScrollorama;
+}($));
